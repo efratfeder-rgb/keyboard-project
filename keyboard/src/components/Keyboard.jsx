53e509cce6d6e97070d30cb64cfe45a1cd969475
efratfeder-rgb <efratfeder@hilma.tech>
2025-10-29 14:49:00 +0200

    f

--- keyboard/src/components/Keyboard.jsx
+++ keyboard/src/components/Keyboard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 function Keyboard(props) {
-  const [language, setLanguage] = useState();
+  const [language, setLanguage] = useState("hebrew");
   const hebrew = [
     "א",
     "ב",
@@ -75,6 +75,32 @@ function Keyboard(props) {
     "💖",
     "🌟",
   ];
-  return <></>;
+  const addNumbers = () => {
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    return numbers.map((num, index) => <button key={index}>{num}</button>);
+  };
+  const generateButtons = (arr) => {
+    return arr.map((item, index) => <button key={index}>{item}</button>);
+  };
+  const addEmojis = () => {
+    return emojis.map((emoji, index) => <button key={index}>{emoji}</button>);
+  };
+
+  return (
+    <>
+      <button>⌫</button>
+      <button>🗑️</button>
+      {addNumbers()}
+      {<br />}
+      {language === "hebrew" && generateButtons(hebrew)}
+      {language === "english" && generateButtons(english)}
+      {language === "emojis" && addEmojis()}
+      <div>
+        <button onClick={() => setLanguage("hebrew")}>עברית</button>
+        <button onClick={() => setLanguage("english")}>English</button>
+        <button onClick={() => setLanguage("emojis")}>אימוג'ים</button>
+      </div>
+    </>
+  );
 }
 export default Keyboard;
