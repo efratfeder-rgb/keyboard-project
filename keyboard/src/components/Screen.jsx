4d9fb79aefc2daa38de2880fab764655336a6ce0
ormiller-hilma <ormiller@hilma.tech>
2025-10-29 14:09:21 +0200

    f

--- keyboard/src/components/Screen.jsx
+++ keyboard/src/components/Screen.jsx
@@ -1,7 +1,17 @@
 function Screen(props) {
+  //   const keyboardString = props.keyboardString;
+  const styles = [{ backgroundColor: "yellow" }, {}, {}]; //props.styles
+  const keyboardString = "!lalala!ff!";
+
+  const keyboardStringSplit = keyboardString.split("!");
+
   return (
     <>
-      <p>lala</p>
+      <p>
+        {keyboardStringSplit.map((text, index) => {
+          <span style={styles[index]}>{text}</span>;
+        })}
+      </p>
     </>
   );
 }
