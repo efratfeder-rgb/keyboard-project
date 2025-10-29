74c7dc24927cbfefa2b3e8786301412c6c9c2e39
ormiller-hilma <ormiller@hilma.tech>
2025-10-29 12:48:08 +0200

    f

--- keyboard/src/App.jsx
+++ keyboard/src/App.jsx
@@ -2,7 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [keyboardString, setKeyboardString] = useState("");
+
+  function updateString(string) {
+    setKeyboardString(string);
+  }
+
+  return (
+    <>
+      <Screen keyboardString={keyboardString} />
+      <Keyboard updateString={(string) => updateString(string)} />
+    </>
+  );
 }
 
 export default App;
